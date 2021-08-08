5829910fdf2696f7b818ac485b6e6e2c253fa92a
InnaKovshik <kovshikinna@gmail.com>
2021-08-08 03:19:19 +0300

    fix

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -39,8 +39,8 @@ class ContactForm extends Component {
           onChange={this.handleChenge}
           type="text"
           name="name"
-          pattern=""
-          title=""
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
           required
         />
         <label className={s.labelNamber}>Number</label>
@@ -50,8 +50,8 @@ class ContactForm extends Component {
           onChange={this.handleChenge}
           type="tel"
           name="number"
-          pattern=""
-          title=""
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
           required
         />
         <button type="submit" className={s.button}>
